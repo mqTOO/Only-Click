81ffc7c2141693ba4924dadc1f8d3cf61ffb78a3
Timurchek <108658768+mqTOO@users.noreply.github.com>
2024-12-15 03:06:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const leaderboardList = document.getElementById("leaderboard-list");
 
     // Инициализация Telegram Web App API
-    const tg = window.Telegram.WebApp;
 
-    // Получаем имя пользователя из Telegram Web App
-    const userName = tg.initDataUnsafe?.user?.username || 'Игрок';
 
     // Получаем данные о кликах из localStorage или устанавливаем 0 по умолчанию
     let coins = parseInt(localStorage.getItem("coins")) || 0;
